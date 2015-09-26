e28e84157d670c63ff03b9ad69164c21be8fb0ae
mperdeck <mperdeck@gmail.com>
2015-09-26 22:10:19 +1000

    initial table

---
+++ MvcApplication1/MvcApplication1/MvcApplication1Context.cs
@@ -0,0 +1,18 @@
+﻿using MvcApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1
+{
+    public class MvcApplication1Context: DbContext
+    {
+        public MvcApplication1Context(string connString): base(connString)
+        {
+        }
+        
+        public DbSet<Book> Books { get; set; } 
+    }
+}
