267d7080c1bb0e9994a3cd5edf1d063fa60e8efe
mperdeck <mperdeck@gmail.com>
2016-05-14 20:34:57 +1000

    app setting sections

--- ImportInWebConfig/SimpleSiteWithDb/SimpleSiteWithDb/Controllers/BooksController.cs
+++ ImportInWebConfig/SimpleSiteWithDb/SimpleSiteWithDb/Controllers/BooksController.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Configuration;
 using System.Web.Configuration;
+using System.Collections.Specialized;
 
 namespace SimpleSiteWithDb.Controllers
 {
@@ -31,8 +32,15 @@ namespace SimpleSiteWithDb.Controllers
         {
             using (var db = new BookStoreContext("BookStoreConnectionString"))
             {
+                NameValueCollection test = (NameValueCollection)ConfigurationManager.GetSection("genericAppSettings");
+                NameValueCollection test2 = (NameValueCollection)ConfigurationManager.GetSection("genericAppSettings2");
+
+                string a = test["another"];
+
+
+
                 string testkey = WebConfigurationManager.AppSettings["TestKey2"];
-                book.Name += testkey;
+                book.Name += testkey + a;
 
 
                 db.Books.Add(book);
