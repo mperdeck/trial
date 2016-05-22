1138160cc41e407a1198c660f444ec9fa90ac920
mperdeck <mperdeck@gmail.com>
2016-05-23 00:28:19 +1000

    ADD INIT

--- ImportInWebConfig/SimpleSiteWithDb/SimpleSiteWithDb/Global.asax.cs
+++ ImportInWebConfig/SimpleSiteWithDb/SimpleSiteWithDb/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BlueChilli.CTARS.Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,6 +16,7 @@ namespace SimpleSiteWithDb
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            DatabaseInitialization.Initialize();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
