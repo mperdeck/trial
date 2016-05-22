e95722543813df5654ae4799ba8ff66fb276fe0f
mperdeck <mperdeck@gmail.com>
2016-05-23 00:51:17 +1000

    set data loss = true

--- ImportInWebConfig/SimpleSiteWithDb/SimpleSiteWithDb/Migrations/Configuration.cs
+++ ImportInWebConfig/SimpleSiteWithDb/SimpleSiteWithDb/Migrations/Configuration.cs
@@ -10,6 +10,7 @@ namespace SimpleSiteWithDb.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(SimpleSiteWithDb.BookStoreContext context)
