855ec627e7f1bb87235aec0f44b6263b765917c5
Matt Perdeck <mperdeck@gmail.com>
2016-07-20 23:22:06 +1000

    1

--- CustomTemplates/UnitTestProject1/TestBase.cs
+++ CustomTemplates/UnitTestProject1/TestBase.cs
@@ -3,17 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using NUnit.Framework;
+using System.Diagnostics;
 
 namespace UnitTestProject1
 {
     [TestFixture]
     public class TestBase
     {
+        public IWebDriver Driver
+        {
+            get; private set;
+        }
+
+        private Process _serverProcess;
+
         [SetUp]
         public void Init()
         {
-            /* ... */
+            _serverProcess = Process.Start(new ProcessStartInfo
+            {
+                FileName = "dotnet",
+                Arguments = "run",
+                WorkingDirectory = jsnlogTestSiteProjectDirectory
+            });
+
+
+            // ################## start iisexpress here
+
+            // ###### install on teamcity: iisexpress, sql server express, chrome
+
+
+            //var executingAssembly = Assembly.GetExecutingAssembly();
+            //var executingAssemblyLocation = executingAssembly.Location;
+            //string assemblyFolder = Path.GetDirectoryName(executingAssemblyLocation);
+            //string dependenciesFolder = Path.Combine(assemblyFolder, "IntegrationTests", "Dependencies");
+
+            Driver = new ChromeDriver(dependenciesFolder);
+
         }
 
         [TearDown]
