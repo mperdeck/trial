01bf04fcd336d85675eaf37cd5ad067101052c74
mperdeck <matt.perdeck@readify.net>
2017-05-20 23:00:34 +1000

    unknown

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using jsnlog;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string x = ">>>>>> Server side logging" + Class1.Get("rrr");
+
+
+
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
