21c16beb54ead73dde8ec1e84d36badbe54f7622
mperdeck <matt.perdeck@readify.net>
2017-05-25 20:43:38 +1000

    Revert "ey3"
    This reverts commit d72ebc12b5919c707259c775b0324dfacececf7e.

--- yt-webpack2-basics-05-webpack-babel-scss-basic-finished/webpack.config.js
+++ yt-webpack2-basics-05-webpack-babel-scss-basic-finished/webpack.config.js
@@ -1,6 +1,10 @@
 var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+var extractPlugin = new ExtractTextPlugin({
+   filename: 'main.css'
+});
+
 module.exports = {
     entry: './src/js/app.js',
     output: {
