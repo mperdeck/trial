b0607e2cf7baf679fdbdcc381a34c7221ac12004
mperdeck <matt.perdeck@readify.net>
2017-09-29 23:37:43 +1000

    1

---
+++ eventlogreader/eventcopier/eventcopier/EventLogHelpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Eventing.Reader;
+
+namespace eventcopier
+{
+    public class EventLogHelpers
+    {
+        public static void Subscribe(Action<Object> eventLogged, params string[] logName)
+        {
+            var eventLogWatcher = new EventLogWatcher("Application");
+            eventLogWatcher.EventRecordWritten += eventLoggedHandler;
+
+
+
+        }
+
+        public static void eventLoggedHandler(object sender, EventRecordWrittenEventArgs e)
+        {
+            return;
+        }
+    }
+}
