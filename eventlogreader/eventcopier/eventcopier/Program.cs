561cb497645f32946b35a5d6d2c3e9a30ede8aa8
mperdeck <matt.perdeck@readify.net>
2017-09-30 08:46:51 +1000

    2 initial attempt

--- eventlogreader/eventcopier/eventcopier/Program.cs
+++ eventlogreader/eventcopier/eventcopier/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics.Eventing.Reader;
 
 namespace eventcopier
 {
@@ -10,6 +11,20 @@ namespace eventcopier
     {
         static void Main(string[] args)
         {
+            var eventLogWatcher = new EventLogWatcher("Application");
+            eventLogWatcher.EventRecordWritten += eventLoggedHandler;
+
+
+            Console.WriteLine("listening for events. Hit any key to exit.");
+            Console.ReadKey();
+        }
+
+        public static void eventLoggedHandler(object sender, EventRecordWrittenEventArgs e)
+        {
+            var id = e.EventRecord.Id;
+
+            Console.WriteLine($"got event {id}");
+            return;
         }
     }
 }
