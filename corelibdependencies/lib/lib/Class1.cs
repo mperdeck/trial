58496bb97358826aebc2a268337e86c7343f7270
mperdeck <matt.perdeck@readify.net>
2017-11-18 17:33:04 +1100

    corelibdependencies initial

---
+++ corelibdependencies/lib/lib/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lib
+{
+    public class Class1
+    {
+        public static int TestInt()
+        {
+            return 66;
+        }
+
+        public static int TestIntB()
+        {
+            return 166;
+        }
+    }
+}
+
+
---
+++ corelibdependencies/lib2/lib2/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using lib;
+
+namespace lib2
+{
+    public class Class2
+    {
+        public int TestInt2()
+        {
+            return (2 * Class1.TestIntB());
+
+        }
+
+
+    }
+}
