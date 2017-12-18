86f21d5c863bc0e3b405d812b5859a105ed934a0
mperdeck <matt.perdeck@readify.net>
2017-12-18 21:11:12 +1100

    testing8incore init

---
+++ testingincore/nunit2/ClassLibrary1/ClassLibrary1.Tests/Class1Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using ClassLibrary1;
+
+namespace ClassLibrary1.Tests
+{
+    [TestFixture]
+    public class Class1Test
+    {
+
+        [Test]
+        public void Test1()
+        {
+            int i = Class1.F1();
+
+            Assert.AreEqual(i,5);
+        }
+    }
+}
