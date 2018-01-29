6a8d7c841f1802a8cdb670fbe5453373ccfeb18d
mperdeck <matt.perdeck@readify.net>
2018-01-29 23:52:58 +1100

    stuff

---
+++ testingincore/nunit2c/ClassLibrary1/ClassLibrary1.Tests/Class1Test.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+using ClassLibrary1;
+using ClassLibrary2.TestLib;
+
+namespace ClassLibrary1.Tests
+{
+    [TestFixture]
+    public class Class1Test
+    {
+
+        [Test]
+        public void Test1()
+        {
+            int i = Class1.F1();
+
+            Class1t.t1(i,9);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            int i = Class1.F1();
+
+            Assert.AreEqual(i, 6);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            int i = Class1.F1();
+
+            Assert.AreEqual(i, 6);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            int i = Class1.F1();
+
+            Assert.AreEqual(i, 6);
+        }
+    }
+}
---
+++ testingincore/nunit2c/ClassLibrary1/ClassLibrary2.TestLib/Class1t.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ClassLibrary2.TestLib
+{
+    public class Class1t
+    {
+        public static void t1(int x, int y)
+        {
+            Assert.AreEqual(x, y);
+
+        }
+
+
+
+    }
+}
