9954d7b296639fcac9297c95b097980d4e6ff847
mperdeck <matt.perdeck@readify.net>
2018-02-10 12:19:55 +1100

    add multiple filters into one attribute

--- filters/filters/sample/src/FiltersSample/Filters/AddHeaderAttribute.cs
+++ filters/filters/sample/src/FiltersSample/Filters/AddHeaderAttribute.cs
@@ -1,8 +1,9 @@
 using Microsoft.AspNetCore.Mvc.Filters;
+using System;
 
 namespace FiltersSample.Filters
 {
-    public class AddHeaderAttribute : ResultFilterAttribute
+    public class AddHeaderAttribute : Attribute, IResultFilter, IActionFilter, IAuthorizationFilter
     {
         private readonly string _name;
         private readonly string _value;
@@ -13,11 +14,29 @@ namespace FiltersSample.Filters
             _value = value;
         }
 
-        public override void OnResultExecuting(ResultExecutingContext context)
+        public void OnResultExecuting(ResultExecutingContext context)
         {
             context.HttpContext.Response.Headers.Add(
                 _name, new string[] { _value });
-            base.OnResultExecuting(context);
+        }
+
+        public void OnResultExecuted(ResultExecutedContext context)
+        {
+            // do something after the action executes
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            // do something before the action executes
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            // do something after the action executes
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
         }
     }
 }
