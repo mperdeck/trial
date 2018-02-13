2741456fd033f050c1df1cbf3e099a53eae80143
mperdeck <matt.perdeck@readify.net>
2018-02-13 22:15:04 +1100

    required checking first try

--- coreconfig/ObjectGraph/Program.cs
+++ coreconfig/ObjectGraph/Program.cs
@@ -2,6 +2,12 @@
 using System.IO;
 using Microsoft.Extensions.Configuration;
 
+// <<<<<<<<<<<<<<
+using System.Reflection;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+
+
 public class Program
 {
     public static void Main(string[] args = null)
@@ -15,10 +21,29 @@ public class Program
         var appConfig = new AppSettings();
         config.GetSection("App").Bind(appConfig);
 
+        EnforceRequiredStrings(appConfig);// <<<<<<<<<<<<<<
+
+
+        // --------------------
+
         Console.WriteLine($"Height {appConfig.Window.Height}");
         Console.WriteLine();
 
         Console.WriteLine("Press a key...");
         Console.ReadKey();
     }
+
+    private static void EnforceRequiredStrings(object options)// <<<<<<<<<<<<<<
+    {
+        var properties = options.GetType().GetTypeInfo().DeclaredProperties.Where(p => p.PropertyType == typeof(string));
+        var requiredProperties = properties.Where(p => p.CustomAttributes.Any(a => a.AttributeType == typeof(RequiredAttribute)));
+
+        foreach (var property in requiredProperties)
+        {
+            if (string.IsNullOrEmpty((string)property.GetValue(options)))
+                throw new ArgumentNullException(property.Name);
+        }
+    }
+
+
 }
