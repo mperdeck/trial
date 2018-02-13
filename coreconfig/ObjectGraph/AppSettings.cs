fb850039b6b9bd281eb4d2926a694bfed6dff912
mperdeck <matt.perdeck@readify.net>
2018-02-13 22:03:23 +1100

    add variety of fields that are not given value in config file

--- coreconfig/ObjectGraph/AppSettings.cs
+++ coreconfig/ObjectGraph/AppSettings.cs
@@ -9,11 +9,17 @@ public class Window
 {
     public int Height { get; set; }
     public int Width { get; set; }
+    public int Depth { get; set; }
+    public int Depth2 { get; set; } = 45;
+    public bool b { get; set; }
+    public bool b2 { get; set; } = true;
 }
 
 public class Connection
 {
     public string Value { get; set; }
+    public string Value2 { get; set; }
+    public string Value3 { get; set; } = "this is value 3";
 }
 
 public class Profile
