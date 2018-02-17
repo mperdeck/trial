44f41e8de8d0dfe1aec74646d77c8b0b4e79bae0
mperdeck <matt.perdeck@readify.net>
2018-02-17 14:12:45 +1100

    working sub apps

--- virtual/core1/core1/Controllers/HomeController.cs
+++ virtual/core1/core1/Controllers/HomeController.cs
@@ -6,18 +6,84 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using core1.Models;
 
+//#################
+using Microsoft.AspNetCore.Http.Extensions;
+
+
 namespace core1.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
+            var p = HttpContext.Request.Path;
+            var p2 = HttpContext.Request.PathBase;
+            ViewBag.p1 = Url.Content("~/");
+            ViewBag.p2 = p.Value;
+            ViewBag.p3 = p2.Value;
+            ViewBag.p4 = HttpContext.Request.GetDisplayUrl();
+            ViewBag.p5 = HttpContext.Request.GetEncodedUrl();
+
+
             return View();
         }
 
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
+            var p = HttpContext.Request.Path;
+            var p2 = HttpContext.Request.PathBase;
+            ViewBag.p1 = Url.Content("~/");    // virtual path, eg. /core1
+            ViewBag.p2 = p.Value;    // absolutpath, eg. /Home/About
+
+            ViewBag.p3 = p2.Value;    // virtual path, eg. /core1 (but if not in sub app, this is empty, while ~ gives /)
+
+            //core: no sub app
+            //p1: /
+            //p2: /Home/About
+            //p3:
+            //p4: http://localhost:8200/Home/About
+            //p5: http://localhost:8200/Home/About
+
+            //core sub app:
+            /*
+             * p1: /core1/
+
+            p2:
+
+            p3: /core1
+
+            p4: http://localhost:8202/core1
+
+            p5: http://localhost:8202/core1
+
+
+            Microsoft Azure
+            Learn how Microsoft's Azure cloud platform allows you to build, deploy, and scale web apps. Learn More
+
+            -----------------------------------
+
+            p1: /core1/
+
+p2: /Home/About
+
+p3: /core1
+
+p4: http://localhost:8202/core1/Home/About
+
+p5: http://localhost:8202/core1/Home/About
+
+             * 
+             * */
+
+
+            // note that for non-core 
+            //application path: /noncore1
+            //HttpRuntime.AppDomainAppVirtualPath = /noncore1
+
+
+            ViewBag.p4 = HttpContext.Request.GetDisplayUrl(); // full url, eg. http://localhost:8202/core1/Home/About
+            ViewBag.p5 = HttpContext.Request.GetEncodedUrl();
 
             return View();
         }
