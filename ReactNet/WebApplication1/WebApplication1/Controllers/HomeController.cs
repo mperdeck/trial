6e7fa3450880f93f7ba9ad6fc4b3577fdcc3254a
mperdeck <matt.perdeck@readify.net>
2018-03-07 23:23:00 +1100

    add comments api endpoint that serves up the comments

--- ReactNet/WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ ReactNet/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -5,11 +5,48 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Models;
+using ReactDemo.Models;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly IList<CommentModel> _comments;
+
+        static HomeController()
+        {
+            _comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Id = 1,
+                    Author = "Daniel Lo Nigro",
+                    Text = "Hello ReactJS.NET World!"
+                },
+                new CommentModel
+                {
+                    Id = 2,
+                    Author = "Pete Hunt",
+                    Text = "This is one comment"
+                },
+                new CommentModel
+                {
+                    Id = 3,
+                    Author = "Jordan Walke",
+                    Text = "This is *another* comment"
+                },
+            };
+        }
+
+        [Route("comments")]
+        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+        public ActionResult Comments()
+        {
+            return Json(_comments);
+        }
+
+        // -----------------------
+
         public IActionResult Index()
         {
             return View();
