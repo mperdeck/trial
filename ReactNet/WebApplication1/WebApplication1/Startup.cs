af2710ddcef5248b2a8a1d0a12cc1e85b80dd101
mperdeck <matt.perdeck@readify.net>
2018-03-07 21:49:47 +1100

    completed demo, which doesn't work

--- ReactNet/WebApplication1/WebApplication1/Startup.cs
+++ ReactNet/WebApplication1/WebApplication1/Startup.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+// react
+using Microsoft.AspNetCore.Http;
+using React.AspNet;
+
 namespace WebApplication1
 {
     public class Startup
@@ -21,6 +25,10 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // react
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddReact();
+
             services.AddMvc();
         }
 
@@ -36,6 +44,31 @@ namespace WebApplication1
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
+            // ------------------------------------
+            // react
+
+            // Initialise ReactJS.NET. Must be before static files.
+            app.UseReact(config =>
+            {
+                // If you want to use server-side rendering of React components,
+                // add all the necessary JavaScript files here. This includes
+                // your components as well as all of their dependencies.
+                // See http://reactjs.net/ for more information. Example:
+                //config
+                //  .AddScript("~/Scripts/First.jsx")
+                //  .AddScript("~/Scripts/Second.jsx");
+
+                // If you use an external build too (for example, Babel, Webpack,
+                // Browserify or Gulp), you can improve performance by disabling
+                // ReactJS.NET's version of Babel and loading the pre-transpiled
+                // scripts. Example:
+                //config
+                //  .SetLoadBabel(false)
+                //  .AddScriptWithoutTransform("~/Scripts/bundle.server.js");
+            });
+
+            // ------------------------------------
 
             app.UseStaticFiles();
 
