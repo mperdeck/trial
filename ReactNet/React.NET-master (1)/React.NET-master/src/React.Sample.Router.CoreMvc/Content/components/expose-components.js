7ff11013c647e48145b249dbcb1790460872c519
mperdeck <matt.perdeck@readify.net>
2018-03-07 22:25:45 +1100

    deleted the react.net source code

--- ReactNet/React.NET-master (1)/React.NET-master/src/React.Sample.Router.CoreMvc/Content/components/expose-components.js
+++
@@ -1,5 +0,0 @@
-require('expose-loader?React!react');
-require('expose-loader?ReactDOM!react-dom');
-require('expose-loader?ReactDOMServer!react-dom/server');
-
-require('expose-loader?RootComponent!./home.jsx');
