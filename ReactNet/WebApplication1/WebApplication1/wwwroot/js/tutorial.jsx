05efc1560fde691688262c5a687fec86f1334934
mperdeck <matt.perdeck@readify.net>
2018-03-07 23:48:01 +1100

    add submit form

--- ReactNet/WebApplication1/WebApplication1/wwwroot/js/tutorial.jsx
+++ ReactNet/WebApplication1/WebApplication1/wwwroot/js/tutorial.jsx
@@ -5,6 +5,53 @@ var data = [
     { id: 3, author: "Jordan Walke", text: "This is *another* comment" }
 ];
 
+var CommentForm = React.createClass({
+
+    getInitialState: function () {
+        return { author: '', text: '' };
+    },
+
+    handleAuthorChange: function (e) {
+        this.setState({ author: e.target.value });
+    },
+
+    handleTextChange: function (e) {
+        this.setState({ text: e.target.value });
+    },
+
+    handleSubmit: function (e) {
+        e.preventDefault();
+        var author = this.state.author.trim();
+        var text = this.state.text.trim();
+        if (!text || !author) {
+            return;
+        }
+
+        this.props.onCommentSubmit({ author: author, text: text });
+        this.setState({ author: '', text: '' });
+    },
+
+    render: function () {
+        return (
+            <form className="commentForm" onSubmit={this.handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Your name"
+                    value={this.state.author}
+                    onChange={this.handleAuthorChange}
+                />
+                <input
+                    type="text"
+                    placeholder="Say something..."
+                    value={this.state.text}
+                    onChange={this.handleTextChange}
+                />
+                <input type="submit" value="Post" />
+            </form>
+        );
+    }
+});
+
 var Comment = React.createClass({
 
     rawMarkup: function () {
@@ -62,6 +109,32 @@ var CommentBox = React.createClass({
         xhr.send();
     },
 
+    handleCommentSubmit: function (comment) {
+        var data = new FormData();
+        data.append('author', comment.author);
+        data.append('text', comment.text);
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('post', this.props.submitUrl, true);
+        xhr.onload = function () {
+            this.loadCommentsFromServer();
+        }.bind(this);
+        xhr.send(data);
+    },
+
+    handleCommentSubmit: function (comment) {
+        var data = new FormData();
+        data.append('author', comment.author);
+        data.append('text', comment.text);
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('post', this.props.submitUrl, true);
+        xhr.onload = function () {
+            this.loadCommentsFromServer();
+        }.bind(this);
+        xhr.send(data);
+    },
+
     componentDidMount: function () {
         this.loadCommentsFromServer();
         window.setInterval(this.loadCommentsFromServer, this.props.pollInterval);
@@ -72,12 +145,13 @@ var CommentBox = React.createClass({
             <div className="commentBox">
                 <h1>Comments</h1>
                 <CommentList data={this.state.data} />
+                <CommentForm onCommentSubmit={this.handleCommentSubmit} />
       </div>
         );
     }
 });
 ReactDOM.render(
-    <CommentBox url="/comments" pollInterval={2000} />,
+    <CommentBox url="/comments" submitUrl="/comments/new" pollInterval={2000} />,
     document.getElementById('content')
 );
 
