4f19864c20d981cdd2c9b605685f4ae828664c5a
mperdeck <matt.perdeck@readify.net>
2018-03-07 21:54:46 +1100

    added fixes for the demo. now works

--- ReactNet/WebApplication1/WebApplication1/Startup.cs
+++ ReactNet/WebApplication1/WebApplication1/Startup.cs
@@ -23,13 +23,15 @@ namespace WebApplication1
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             // react
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddReact();
 
             services.AddMvc();
+
+            return services.BuildServiceProvider();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
