cb8c7b76fea893efcb054b9d5bb49c54510cad4f
mperdeck <matt.perdeck@readify.net>
2018-03-07 21:50:20 +1100

    completed demo 2

---
+++ ReactNet/WebApplication1/WebApplication1/wwwroot/js/tutorial.jsx
@@ -0,0 +1,20 @@
+﻿
+// from this tutorial:
+// https://reactjs.net/getting-started/tutorial.html
+
+var CommentBox = React.createClass({
+    render: function () {
+        return (
+            <div className="commentBox">
+                Hello, world! I am a CommentBox.
+      </div>
+        );
+    }
+});
+ReactDOM.render(
+    <CommentBox />,
+    document.getElementById('content')
+);
+
+
+
