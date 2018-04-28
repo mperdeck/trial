150db0e14a9f64991e2ba02153ef86b6ddfda894
mperdeck <matt.perdeck@readify.net>
2018-04-28 15:42:50 +1000

    core service initial

---
+++ corewindowsservice/coreservice/coreservice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace coreservice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
