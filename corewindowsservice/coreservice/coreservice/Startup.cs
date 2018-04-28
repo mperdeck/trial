a9201b175dd4dd5bfc12535cf77163cef4a818b9
mperdeck <matt.perdeck@readify.net>
2018-04-28 19:27:12 +1000

    core ws cleanup 2

--- corewindowsservice/coreservice/coreservice/Startup.cs
+++ corewindowsservice/coreservice/coreservice/Startup.cs
@@ -23,7 +23,7 @@ namespace coreservice
 
         }
 
-        public IConfigurationRoot Configuration { get; }
+     //   public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
