1fc6d25f82584fcb469af8f0dbe767177f6f19c9
mperdeck <matt.perdeck@readify.net>
2018-05-05 18:09:45 +1000

    also doesn't work

--- React/ReactAndWebpack/proj/webpack.config.js
+++ React/ReactAndWebpack/proj/webpack.config.js
@@ -1,9 +1,5 @@
 module.exports = {
     entry: "./src/index.tsx",
-    output: {
-        filename: "bundle.js",
-        path : path.resolve(__dirname, 'dist')
-    },
 
     // Enable sourcemaps for debugging webpack's output.
     devtool: "source-map",
