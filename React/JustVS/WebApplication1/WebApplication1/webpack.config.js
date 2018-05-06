8dccdc46d9e067386306fda713a1b2756d0eff44
mperdeck <matt.perdeck@readify.net>
2018-05-06 23:23:49 +1000

    keep react etc. out of the bundle

--- React/JustVS/WebApplication1/WebApplication1/webpack.config.js
+++ React/JustVS/WebApplication1/WebApplication1/webpack.config.js
@@ -5,10 +5,19 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/module.js',
     output: {
         // the code "path.resolve(__dirname, 'wwwroot/js')" doesn't work, so just append the '/js'
         path: path.resolve(__dirname, 'wwwroot') + '/js',
         filename: 'main.js'
+    },
+
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
     }
 };
