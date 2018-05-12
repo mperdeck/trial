5b91c67cc806abff190ea5e7a78e14c5680507f7
mperdeck <matt.perdeck@readify.net>
2018-05-12 21:53:54 +1000

    1

--- React/JustVS/WebApplication1/WebApplication1/src/module.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-alert(96609);
-//ffffggg 
-//# sourceMappingURL=module.js.map
--- React/JustVS/WebApplication1/WebApplication1/wwwroot/js/Views/Home/index.js
+++ React/JustVS/WebApplication1/WebApplication1/wwwroot/js/Views/Home/index.js
@@ -1,31 +1,21 @@
 import * as React from "../../node_modules/@types/react/index";
 import * as ReactDOM from "../../node_modules/@types/react-dom/index";
-
 /// <r eference path="../../node_modules/@types/react-dom/index.d.ts" />
-
 import { Hello } from "../../src/components/Hello.js";
 // Must use .js extension, because this line will be used by the browser and it requires an extension
-
 // this works for both compiler and browser
 //declare var ReactDOM: any;
 //declare var React: any;
-
-
 //mmmllldddhhhh445578866
-
 (function () {
     var elements = document.getElementsByClassName("example");
-    var i: number;
-    var compiler: string;
-    var framework: string;
+    var i;
+    var compiler;
+    var framework;
     for (i = 0; i < elements.length; i++) {
-        compiler = (elements[i] as any).dataset.compiler;
-        framework = (elements[i] as any).dataset.framework;
-
-        ReactDOM.render(
-            <Hello compiler={compiler} framework={framework} />, elements[i]
-        );
+        compiler = elements[i].dataset.compiler;
+        framework = elements[i].dataset.framework;
+        ReactDOM.render(React.createElement(Hello, { compiler: compiler, framework: framework }), elements[i]);
     }
 })();
-
-
+//# sourceMappingURL=index.js.map
