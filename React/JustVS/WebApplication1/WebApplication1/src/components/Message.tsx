32686d500ec74f2d61b9924ce6236aa05ca6d168
mperdeck <matt.perdeck@readify.net>
2018-05-13 15:19:50 +1000

    doesn't work

--- React/JustVS/WebApplication1/WebApplication1/src/components/Message.tsx
+++ React/JustVS/WebApplication1/WebApplication1/src/components/Message.tsx
@@ -2,9 +2,17 @@
 //declare var ReactDOM: any;
 //declare var React: any;
 
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from "./react-types.js";
+import * as ReactDOM from "./react-dom-types.js";
 
+
+//import * as React from "react";
+//import * as ReactDOM from "react-dom";
+
+
+
+if (false) {
+}
 
 export interface MessageProps { children: string[] }
 
---
+++ React/JustVS/WebApplication1/WebApplication1/src/components/react-dom-types.js
@@ -0,0 +1,2 @@
+export * as "ReactDOM" from "react-dom";
+
