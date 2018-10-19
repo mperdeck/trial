8ced71907a15098dceb5e27f8376932d11c53637
mperdeck@gmail.com <mperdeck@gmail.com>
2018-10-20 09:09:28 +1100

    ef - add packages and tools

--- efcore/ConsoleApp1/ConsoleApp1/Program.cs
+++ efcore/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 
+// https://docs.microsoft.com/en-us/ef/core/
+
+// generate the poco classes:
+// https://wildermuth.com/2017/12/20/Reverse-Engineering-Existing-Databases-in-Entity-Framework-Core-2
+
 namespace ConsoleApp1
 {
     class Program
